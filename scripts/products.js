5d1c154e8286deaa5ed3372d91300ee47e11f01e
iCodeIX <sherwincarlosph@gmail.com>
2022-07-02 20:51:12 +0800

    structure of products list done with options

---
+++ scripts/products.js
@@ -0,0 +1,63 @@
+let products = [
+        {
+            "product_id":1,
+            "product_name":"Violet mat",
+            "product_price":123,
+            "product_image":"images/products/product_1.jpg"
+        },
+        {
+            "product_id":2,
+            "product_name":"Dumbell and iron push ",
+            "product_price":124,
+            "product_image":"images/products/product_2.jpg"
+        },
+        {
+            "product_id":3,
+            "product_name":"Large dumbell",
+            "product_price":123412,
+            "product_image":"images/products/product_3.jpg"
+        },
+        {
+            "product_id":4,
+            "product_name":"Medium dumbell green",
+            "product_price":1232,
+            "product_image":"images/products/product_4.jpg"
+        },
+        {
+            "product_id":5,
+            "product_name":"Small dumbell silver",
+            "product_price":12344,
+            "product_image":"images/products/product_5.jpg"
+        },
+        {
+            "product_id":6,
+            "product_name":"Jumping rope with blue handle",
+            "product_price":12223,
+            "product_image":"images/products/product_6.jpg"
+        },
+        {
+            "product_id":7,
+            "product_name":"Finger pressers",
+            "product_price":1223,
+            "product_image":"images/products/product_7.jpg"
+        },
+        {
+            "product_id":8,
+            "product_name":"Lift the ball",
+            "product_price":1203,
+            "product_image":"images/products/product_8.jpg"
+        },
+        {
+            "product_id":9,
+            "product_name":"Weighing scale with free tape measure",
+            "product_price":12973,
+            "product_image":"images/products/product_9.jpg"
+        },
+        {
+            "product_id":10,
+            "product_name":"Thin blue jumping rope",
+            "product_price":1263,
+            "product_image":"images/products/product_10.jpg"
+        }
+
+    ]
--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,95 @@ const hamburgerIcon = document.querySelector(".hamburger-icon");
 const closeNavIcon = document.querySelector(".close-nav-icon");
 const navMenu = document.querySelector(".header-nav");
 
+/*products variables */
+
+
+/* HEADER AND NAVIGATION */
+
 hamburgerIcon.addEventListener("click", showNavMenu);
 closeNavIcon.addEventListener("click", closeNavMenu);
 
-function showNavMenu(){
-    navMenu.style.display = "block"; 
+function showNavMenu() {
+    navMenu.style.display = "block";
 }
 
-function closeNavMenu(){
+function closeNavMenu() {
     navMenu.style.display = "none";
 }
+
+/* PRODUCTS SECTION */
+
+//populate products list with items
+
+const productsList = document.querySelector(".products-list");
+
+for (let i = 0; i < products.length; i++) {
+
+    let productItem = document.createElement("li");
+    productItem.className = "product-item";
+
+    let productImage = document.createElement("img");
+    productImage.className = "product-image";
+    productImage.src = products[i].product_image;
+
+
+    let productName = document.createElement("p");
+    productName.className = "product-name";
+    productName.innerText = products[i].product_name;
+
+    let productPrice = document.createElement("p");
+    productPrice.className = "product-price";
+    productPrice.innerText = products[i].product_price;
+
+    let productQuantityToBuy = document.createElement("input");
+    productQuantityToBuy.type = "number";
+    productQuantityToBuy.className = "quantity-to-buy";
+    productQuantityToBuy.value = 1;
+
+    let productDecrementQuantityToBuyBtn = document.createElement("button");
+    productDecrementQuantityToBuyBtn.className = "quantity-decrease-btn";
+    productDecrementQuantityToBuyBtn.innerText = "-";
+
+    let productIncrementQuantityToBuyBtn = document.createElement("button");
+    productIncrementQuantityToBuyBtn.className = "quantity-increase-btn";
+    productIncrementQuantityToBuyBtn.innerText = "+";
+
+    let addToCartBtn = document.createElement("button");
+    addToCartBtn.className = "product-add-cart-btn";
+    addToCartBtn.innerText = "Add to cart";
+
+    productItem.append(productImage, productName, productPrice, productDecrementQuantityToBuyBtn
+        , productQuantityToBuy, productIncrementQuantityToBuyBtn, addToCartBtn);
+    productsList.append(productItem);
+
+}
+
+// increment and decrement of quantity to buy
+
+const incrementBtn = document.querySelectorAll(".quantity-increase-btn");
+var quantity = 1;
+
+//increment buttons 
+incrementBtn.forEach((item, index) => {
+    item.addEventListener('click', arrow => {
+        quantity = parseInt(incrementBtn[index].previousElementSibling.value);
+        quantity++;
+        incrementBtn[index].previousElementSibling.value = quantity;
+
+    })
+})
+
+const decrementBtn = document.querySelectorAll(".quantity-decrease-btn");
+
+//decrement buttons
+decrementBtn.forEach((item, index) => {
+    item.addEventListener('click', arrow => {
+        quantity = parseInt(decrementBtn[index].nextElementSibling.value);
+        if (quantity != 1) {
+            quantity--;
+            decrementBtn[index].nextElementSibling.value = quantity;
+        }
+
+    })
+})
+
